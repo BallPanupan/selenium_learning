72c8175d76b5f3ee412099c0b2aebc76672862eb
Ball Panupan <Ball@reverseads.com>
2022-03-24 15:29:15 +0700

    get table

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function saveLog(data) {
     let milliseconds = new Date().getTime();
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let fileName = milliseconds + ".txt";
-
+    // let fileName = milliseconds + ".txt";
+    let fileName = "GetTable.json";
+    
     fs.writeFile("log/"+fileName, data, function(err) {
         if (err) {
             console.log(err);
@@ -15,25 +16,41 @@ function saveLog(data) {
     });
 }
 
-// process.exit(1)
-
-(async function example() {
+async function example(target) {
     let driver = await new Builder().forBrowser('chrome').build();
     let data = {};
 
     try {
-        await driver.get('http://www.google.com/');
         await driver.getTitle();
-        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.get(target.url);
         
         data['Title-Page'] = await driver.getTitle();
+        data['Element'] = await driver.findElement({className:'tableizer-table sortable'});
 
-        object.push(data);
+        let elements = await driver.findElements(By.className('tableizer-table sortable'));
+
+        // console.log(driver);
+        for(let e of elements) {
+            console.log(await e.getText());
+        }
+
 
     } finally {
         saveLog(JSON.stringify(object, undefined, 4));
-        
         await driver.quit();
     }
 
-})();
+}
+
+// get target
+let target = fs.readFileSync('target.json');
+let targetList = JSON.parse(target);
+
+targetList.map((data, index) => {
+    // console.log(data)
+    example(data);
+
+});
+
+
+// process.exit(1)
