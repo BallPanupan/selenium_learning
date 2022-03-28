d4d49b50210b44cf7d9a04f5eeefed0eb6d1a3f3
Ball Panupan <Ball@reverseads.com>
2022-03-28 10:07:57 +0700

    custom local web for test click to download

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
 
-
 driver = webdriver.Chrome()
+
+driver.get("http://127.0.0.1/)")
+
+driver.implicitly_wait(5)
+my_element = driver.find_element_by_id('downloadButtonx')
+my_element.click()
+ 
+
