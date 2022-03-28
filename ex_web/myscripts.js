3210b281aebbd9944a14b20126491227af193649
Ball Panupan <Ball@reverseads.com>
2022-03-28 10:51:11 +0700

    fix(ex_web): update click to show display none and block of completed element

--- ex_web/myscripts.js
+++ ex_web/myscripts.js
@@ -1,41 +1,50 @@
-// let xxx = document.getElementById('downloadButton')
-// xxx
+window.console = window.console || function (t) {};
+
+if (document.location.search.match(/type=embed/gi)) {
+	window.parent.postMessage("resize", "*");
+}
 
 var rID = null;
 
-var load = function(el, s, t) {
+var load = function (el, s, t) {
 	var perc, k = 2;
-	
-	if(t%k === 0) {
-		perc = t/k;
-		el.dataset.perc = perc + '%';
-		el.style.backgroundSize = 
-			s.replace(/0px/g, el.dataset.perc);
-		
-		if(perc === 100) {
-			cancelAnimationFrame(rID);
-			rID = null;
-			return;
-		}
-	}
-	
-	rID = requestAnimationFrame(
-		load.bind(this, el, s, ++t)
-	);
-};
 
-document.querySelector('button')
-	.addEventListener('click', function() {
-	var s;
-	
-	if(!rID) {
+	if (t % k === 0) {
+	  perc = t / k;
+	  el.dataset.perc = perc + '%';
+	  el.style.backgroundSize =
+		s.replace(/0px/g, el.dataset.perc);
+
+	  if (perc === 100) {
+		cancelAnimationFrame(rID);
+		rID = null;
+		document.getElementById("completed").style.display = "block";
+		return;
+	  }
+	}
+
+	rID = requestAnimationFrame(
+	  load.bind(this, el, s, ++t));
+
+  };
+
+function main(){
+	document.querySelector('button').
+	addEventListener('click', function () {
+		document.getElementById("completed").style.display = "none";
+	  var s;
+  
+	  if (!rID) {
 		this.classList.toggle('loading');
 		s = getComputedStyle(this).backgroundSize;
-		if(this.classList.contains('loading'))
-			load(this, s, 0);
+		if (this.classList.contains('loading'))
+		  load(this, s, 0);
 		else {
-			this.dataset.perc = 
-			this.style.backgroundSize = ''
+		  this.dataset.perc =
+			this.style.backgroundSize = '';
 		}
-	}
-}, false);
+	  }
+	}, false);
+}
+
+  //# sourceURL=pen.js
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("http://127.0.0.1/)")
+driver.get("file:///C:/Users/AsusX/Desktop/my_learning/selenium_learning/ex_web/index.html")
 
-driver.implicitly_wait(5)
+driver.implicitly_wait(5) #time.sleep(5)
 my_element = driver.find_element_by_id('downloadButtonx')
 my_element.click()
  
